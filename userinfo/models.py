e396a1cc5ba8af4f7fa9dfa371df5a4a21716bd7
System Administrator <root@Sai-Kumar-Sura.local>
2018-06-21 13:11:25 +0530

    Registration form completed.

--- userinfo/models.py
+++ userinfo/models.py
@@ -3,3 +3,17 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+import datetime
+from datetime import datetime
+
+class Usersdata(models.Model):
+	id = models.AutoField(primary_key=True,max_length=11) 
+	user_name = models.CharField(max_length=50)
+	last_name = models.CharField(max_length=50)
+	email = models.CharField(max_length=100)
+	password = models.CharField(max_length=40)
+	created_datetime = models.DateTimeField()
+	location = models.CharField(max_length=40)
+	class Meta:
+		managed = False
+		db_table = 'users_data'
